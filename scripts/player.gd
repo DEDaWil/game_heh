0b16bfbfe2457f1e2cf91edbdef992ad08305424
Pitswood <crywayseeker@gmail.com>
2020-06-17 18:37:13 +0200

    Add player_up and jump mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 # export
 export (int) var run_speed = 100
 export (int) var gravity = 1200
-
+export (int) var jump = 500
 # vars
 var velocity = Vector2()
 
@@ -29,6 +29,8 @@ func _physics_process(delta):
 		else:
 			$Animations.play("Idle")
 			velocity.x = 0
+	if Input.is_action_pressed("player_up")&& is_on_floor():
+		velocity.y = -jump
 	
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1))
