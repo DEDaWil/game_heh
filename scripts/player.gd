11b34cb97c4ada72dde5457450585d728ae25978
Ilia Bardinov <ilya.bardinov@gmail.com>
2020-06-17 17:28:02 +0200

    add gravity and collision to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-# consts
-const SPEED = 200
+# export
+export (int) var run_speed = 100
+export (int) var gravity = 1200
 
 # vars
 var velocity = Vector2()
@@ -11,7 +12,7 @@ var velocity = Vector2()
 #	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _physics_process(delta):
 	if Input.is_action_just_pressed("player_hit"):
 		$Animations.play("Hit")
 		velocity.x = 0
@@ -20,13 +21,14 @@ func _process(_delta):
 		if Input.is_action_pressed("player_right"):
 			$Animations.play("Run")
 			$Sprite.flip_h = false
-			velocity.x = SPEED
+			velocity.x = run_speed
 		elif Input.is_action_pressed("player_left"):
 			$Animations.play("Run")
 			$Sprite.flip_h = true
-			velocity.x = -SPEED
+			velocity.x = -run_speed
 		else:
 			$Animations.play("Idle")
 			velocity.x = 0
 	
+	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1))
