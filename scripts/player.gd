86f17c6e07a6021751b9ba4d10010491d3fbb27e
Ilia Bardinov <ilya.bardinov@gmail.com>
2020-06-19 21:53:24 +0200

    fix layers of scenes, fix take damage animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var Consts = preload("res://scripts/core/consts.gd")
+
 signal update_hud(health)
 
 # export
@@ -11,6 +13,7 @@ export (int) var health = 3
 
 # vars
 var velocity = Vector2()
+var direction = Consts.Direction.Left
 
 func _ready():
 	emit_signal("update_hud", health)
@@ -20,34 +23,35 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("player_hit"):
 		$Animations.play("Hit")
 		velocity.x = 0
-				
+
 	if $Animations.current_animation != "Hit":
 		if Input.is_action_pressed("player_right"):
-			$Animations.play("Run")
-			$RayCast2D.cast_to.x = abs($RayCast2D.cast_to.x)
-			$Sprite.flip_h = false
-			velocity.x = run_speed
+			run(Consts.Direction.Left)
 		elif Input.is_action_pressed("player_left"):
-			$Animations.play("Run")
-			$RayCast2D.cast_to.x = -1 * abs($RayCast2D.cast_to.x)
-			$Sprite.flip_h = true
-			velocity.x = -run_speed
+			run(Consts.Direction.Right)
 		else:
 			$Animations.play("Idle")
 			velocity.x = 0
 			
-	if Input.is_action_pressed("player_up") && is_on_floor():
-		velocity.y = -jump
-	
+		if Input.is_action_pressed("player_up") && is_on_floor():
+			velocity.y = -jump
+
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 
 	detect_weapon_raycasting()
+
+func run(_direction):
+	direction = _direction
+	$Animations.play("Run")
+	$RayCast2D.cast_to.x = direction * abs($RayCast2D.cast_to.x)
+	$Sprite.flip_h = direction == -1
+	velocity.x = direction * run_speed
 
 func detect_weapon_raycasting():
 	if $RayCast2D.enabled:
 		var target = $RayCast2D.get_collider()
 		if target != null:
 			if target.has_method("take_damage"):
-				target.take_damage(damage)
+				target.take_damage(damage, direction)
 				$RayCast2D.enabled = false
--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const constants = preload("res://scripts/core/consts.gd");
+
 # signals
 signal death
 signal health_change
@@ -19,34 +21,36 @@ func _ready():
 func _physics_process(delta):
 	if health > 0:
 		if $Animation.current_animation == "TakingDamage":
-			velocity.x = 100 * direction
-			if is_on_floor():
-				velocity.y = -200
-		else:
+			print ("0")
+		elif is_on_floor():
 			velocity.x = speed * direction
 			$Animation.play("Walk")
 	velocity.y += (gravity * delta)
 	velocity = move_and_slide(velocity, Vector2(0,-1))
-	if is_on_wall():
+	if is_on_wall() && is_on_floor():
 		change_direction()
 
 func change_direction():
 	direction *= -1
 	$Sprite.flip_h = !$Sprite.flip_h
 
-func take_damage(damage):
-	$Animation.play("TakingDamage")
+func take_damage(damage, _direction):
+	velocity.x = speed * _direction
+	velocity.y = -200
 	health -= damage
 	emit_signal("health_change")
-	if health <= 0:
-		health = 0
-		emit_signal("death")
 
 func _on_skeleton_death():
+	set_collision_layer_bit(constants.Layers.Enemy, false)
 	velocity.x = 0
-	$CollisionShape2D.set_deferred("disabled", true)
 	$Animation.play("Death")
 
 
 func _on_skeleton_health_change():
+	if health <= 0:
+		health = 0
+		emit_signal("death")
+	else:
+		$Animation.stop()
+		$Animation.play("TakingDamage")
 	$HealthBar.value = health
