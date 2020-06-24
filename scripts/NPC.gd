46516ae49417bdb34bdd338bf737cc3c723dcb18
Ilia Bardinov <ilya.bardinov@gmail.com>
2020-06-24 15:28:53 +0200

    correct position of dialogue bubble

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -1,14 +1,17 @@
-extends KinematicBody2D
+extends Area2D
 
 const Bubble = preload("res://scenes/dialogue_bubble.tscn")
 
-onready var Player = get_node("../Player")
-
 var bubble: Node2D = null
+var updateLayout: bool = false;
 
 func _process(_delta):
+	if updateLayout:
+		updateLayout = false
+		var bubble_sprite: NinePatchRect = bubble.get_node("VBoxContainer/Label/NinePatchRect")
+		bubble.position = position + Vector2(-bubble_sprite.rect_size.x / 2.0, -$Sprite.texture.get_height() / 2.0 * scale.y - bubble_sprite.rect_size.y - 10)
 	if bubble == null:
 		bubble = Bubble.instance()
 		get_parent().add_child(bubble)
 		bubble.get_node("VBoxContainer/Label").text = "Please,\nkill him!"
-		bubble.position = position + Vector2(-bubble.get_node("VBoxContainer").rect_size.x / 2.0, -$Sprite.texture.get_height() - bubble.get_node("VBoxContainer").rect_size.y - 40)
+		updateLayout = true;
