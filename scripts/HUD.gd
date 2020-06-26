f564b563263a5a9561b01a3f09bc074974f6504a
Ilia Bardinov <ilya.bardinov@gmail.com>
2020-06-26 18:29:48 +0200

    add fps counter

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -2,6 +2,10 @@ extends Control
 
 # onready
 onready var HealthBar: TextureProgress = get_node("Health")
+onready var FPS: Label = get_node("FPS")
+
+func _process(delta):
+	FPS.text = "fps: " + str(Engine.get_frames_per_second())
 
 func _on_Player_update_hud(health: int):
 	HealthBar.value = health
