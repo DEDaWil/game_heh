af06de912119cb9a17dc2920670c7898bf9f5c34
Ilia Bardinov <ilya.bardinov@gmail.com>
2020-06-27 22:03:32 +0200

    restart level on player death

--- scripts/level.gd
+++ scripts/level.gd
@@ -3,3 +3,9 @@ extends Node
 func _on_Exit_body_entered(body: Node):
 	if body.is_in_group("Player"):
 		get_tree().change_scene("res://scenes/level_2.tscn")
+
+func _on_Player_death():
+	$LevelRestartTimer.start()
+
+func _on_LevelRestartTimer_timeout():
+	get_tree().change_scene("res://scenes/level.tscn")
